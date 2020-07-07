4310ca6ce170a2cba2431e5d865ed5a0705dfbaf
deepikadipss <vdeepikaveera@gmail.com>
2020-07-08 01:44:04 +0530

    Assignment

---
+++ src/app/data.ts
@@ -0,0 +1,122 @@
+export const FEATURES = [
+  {
+      "service": "Free Pick Up & Drop",
+      "p3": "Yes",
+      "p2": "Yes",
+      "p1": "Yes"
+  },
+  {
+      "service": "Interior Sanitization for Covid-19",
+      "p3": "Yes",
+      "p2": "Yes",
+      "p1": "Yes"
+  },
+  {
+      "service": "Engine Oil Change",
+      "p3": "Yes",
+      "p2": "Yes",
+      "p1": "Yes"
+  },
+  {
+      "service": "Oil Filter Change",
+      "p3": "Yes",
+      "p2": "Yes",
+      "p1": "Yes"
+  },
+  {
+      "service": "Coolant Top-Up (200 ml)",
+      "p3": "Yes",
+      "p2": "Yes",
+      "p1": "Yes"
+  },
+  {
+      "service": "Wiper Fluid Change",
+      "p3": "Yes",
+      "p2": "Yes",
+      "p1": "Yes"
+  },
+  {
+      "service": "Battery Fluids Top-Up",
+      "p3": "Yes",
+      "p2": "Yes",
+      "p1": "Yes"
+  },
+  {
+      "service": "Heater \/ Spark Plugs Cleaning",
+      "p3": "Yes",
+      "p2": "Yes",
+      "p1": "Yes"
+  },
+  {
+      "service": "Car Wash",
+      "p3": "Yes",
+      "p2": "Yes",
+      "p1": "Yes"
+  },
+  {
+      "service": "Air Filter Change",
+      "p3": "Yes",
+      "p2": "Yes",
+      "p1": "Clean"
+  },
+  {
+      "service": "Diagnostic Scanning",
+      "p3": "Yes",
+      "p2": "Yes",
+      "p1": null
+  },
+  {
+      "service": "AC Filter Cleaning",
+      "p3": "Yes",
+      "p2": "Yes",
+      "p1": null
+  },
+  {
+      "service": "Brake Oil Top-Up (50 ml)",
+      "p3": "Yes",
+      "p2": "Yes",
+      "p1": null
+  },
+  {
+      "service": "Brake Pads Cleaning",
+      "p3": "Yes",
+      "p2": "Yes",
+      "p1": null
+  },
+  {
+      "service": "Fuel Filter Change",
+      "p3": "Yes",
+      "p2": "Clean",
+      "p1": null
+  },
+  {
+      "service": "Wheel Balancing Test",
+      "p3": "Yes",
+      "p2": null,
+      "p1": null
+  },
+  {
+      "service": "Wheel Alignment Test",
+      "p3": "Yes",
+      "p2": null,
+      "p1": null
+  },
+  {
+      "service": "Tyre Rotation Test",
+      "p3": "Yes",
+      "p2": null,
+      "p1": null
+  },
+  {
+      "service": "Throttle Body Cleaning",
+      "p3": "Yes",
+      "p2": null,
+      "p1": null
+  },
+  {
+      "service": "Gear Oil Top-Up (50 ml)",
+      "p3": "Yes",
+      "p2": null,
+      "p1": null
+  }
+];
